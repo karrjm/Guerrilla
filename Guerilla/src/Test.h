a87e13a8e14f3149d3205f4a9ab960eb560c5f2f
karrjm <karrjm@gmail.com>
2021-02-17 21:23:37 -0600

    Add project files.

---
+++ Guerilla/src/Test.h
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace GuerillaEngine {
+	__declspec(dllexport) void Print();
+}
---
+++ Sandbox/src/Application.cpp
@@ -0,0 +1,8 @@
+namespace GuerillaEngine {
+	__declspec(dllimport) void Print();
+}
+
+void main() {
+	GuerillaEngine::Print();
+
+}
